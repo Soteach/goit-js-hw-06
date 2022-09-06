01da15759be6f875c871e6f7b3894df13fa82aa6
Sergij Suprunenko <soteach1982@gmail.com>
2022-09-06 17:37:58 +0300

    task-1 done

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const listRef = document.querySelectorAll(".item");
+console.log("Number of categories:", listRef.length);
+
+
+listRef.forEach(item => {
+    console.log("Category:", item.firstElementChild.textContent);
+    console.log("Elements:", item.querySelectorAll("li").length); 
+});
