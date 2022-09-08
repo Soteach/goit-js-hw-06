71c60019a69771bd84b14b705bbbb396257b0a83
Sergij Suprunenko <soteach1982@gmail.com>
2022-09-08 14:32:13 +0300

    make-hw (i hope)

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,26 @@
+const formRef = document.querySelector('.login-form');
 
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(event) {
+  event.preventDefault();
+
+  const elementsForm = event.currentTarget.elements;
+  const email = elementsForm.email.value;
+  const password = elementsForm.password.value;
+  
+
+  if (email === '' || password === '') {
+    alert('Заповніть всі поля!');
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  }
+  
+  console.log(formData);
+
+  event.currentTarget.reset();
+};
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorChangeBtn = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+colorChangeBtn.addEventListener('click', onBtnColorChange);
+
+function onBtnColorChange() {
+  color.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = color.textContent;
+};
